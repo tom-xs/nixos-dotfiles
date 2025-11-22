1e5a701bf5ab7e60a4c71ac93342f5a85898d4cc
Tomas Xavier Santos <tom.xaviersantos@gmail.com>
2025-11-22 17:34:40 -0300

    Add steam configs to configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -104,7 +104,13 @@
   programs.git = {
     enable = true;
   };
-  programs.steam.enable = true;
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
   programs.gamemode.enable = true;
 
   # System
