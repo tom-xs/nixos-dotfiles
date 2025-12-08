6f242cc12f51b77b198e29e1681fb953aa14cd81
Tomas Xavier Santos <tom.xaviersantos@gmail.com>
2025-12-08 15:13:52 -0300

    Add .config options for konsole

--- home/konsole.nix
+++ home/konsole.nix
@@ -1,20 +1,32 @@
 { pkgs, ... }:
 
 {
-  programs.konsole = {
-    enable = true;
-    defaultProfile = "MyProfile";
+  # 1. Ensure Konsole is installed
+  home.packages = [ pkgs.kdePackages.konsole ];
 
-    profiles = {
-      "MyProfile" = {
-        font = {
-          name = "JetBrainsMono Nerd Font";
-          size = 12;
-        };
-        # Using a built-in standard or valid name.
-        # If you need a custom colorscheme file, you'd use 'extraConfig' or 'customColorSchemes'.
-        colorScheme = "Breeze";
-      };
-    };
-  };
+  # 2. Set the default profile in konsolerc
+  xdg.configFile."konsolerc".text = ''
+    [Desktop Entry]
+    DefaultProfile=MyProfile.profile
+
+    [TabBar]
+    ShowTabBar=true
+  '';
+
+  # 3. Create the actual profile file
+  # Konsole looks for profiles in ~/.local/share/konsole/
+  xdg.dataFile."konsole/MyProfile.profile".text = ''
+    [General]
+    Name=MyProfile
+    Parent=FALLBACK/
+
+    [Appearance]
+    # You can change this to "BlackOnWhite", "Dracula", etc. if installed
+    ColorScheme=Breeze 
+    Font=JetBrainsMono Nerd Font,12,-1,5,50,0,0,0,0,0
+
+    [Scrolling]
+    HistoryMode=2
+    ScrollBarPosition=1
+  '';
 }
