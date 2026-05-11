1cc2aee9b51b7beef2490411c61d2a19f956e48b
Tomas Xavier Santos <tom.xaviersantos@gmail.com>
2026-05-11 14:15:55 -0300

    Update wsl configuration

--- hosts/wsl/home.nix
+++ hosts/wsl/home.nix
@@ -1,4 +1,9 @@
-{ pkgs, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 {
   # Change this if your WSL username is different
@@ -16,23 +21,35 @@
     ../../home/shell.nix
     ../../home/tmux.nix
   ];
+  _module.args.neovimNodeTooling = true;
 
-  # 3. WSL Specific Packages (Optional)
+  home.sessionPath = [
+    "${config.home.homeDirectory}/.local/bin"
+    "${config.home.homeDirectory}/go/bin"
+    "/opt/android-studio/bin"
+  ];
+
   home.packages = with pkgs; [
-    wslu # WSL Utilities (wslview, wslact, etc.)
+    # wslu # WSL Utilities (wslview, wslact, etc.)
     wget
     curl
+
+    go
+    gh
   ];
 
   # 4. Git Config
   programs.git = {
     enable = true;
-    settings.user = {
-      name = "Tomas Xavier Santos";
-      email = "tom.xaviersantos@gmail.com";
+    settings = {
+      user = {
+        name = "Tomas Xavier Santos";
+        email = "tom.xaviersantos@gmail.com";
+      };
+      init.defaultBranch = "master";
+      # Optional: Use Windows Credential Manager
+      credential.helper = "/mnt/c/Program\\ Files/Git/mingw64/libexec/git-core/git-credential-manager.exe";
     };
-    # Optional: Use Windows Credential Manager
-    extraConfig.credential.helper = "/mnt/c/Program\\ Files/Git/mingw64/libexec/git-core/git-credential-manager.exe";
   };
 
   # 5. Enable Home Manager
